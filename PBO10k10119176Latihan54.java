9e4a8a7a3eaf4fcc46b345dd985a68272c69dd01
Muhammad Fauzan <muhammadfauzanfadhlulbarr@gmail.com>
2020-11-22 08:59:06 +0700

    first commit

---
+++ PBO10k10119176Latihan54.java
@@ -0,0 +1,19 @@
+/**
+ * Latihan54
+ * Nama     : Muhammad Fauzan Fadhlulbarr
+ * Kelas    : PBO10K
+ * NIM      : 10119176
+ * Program  : Koordinat
+ */
+public class PBO10k10119176Latihan54 {
+
+        public static void main(String[] args) {
+            // TODO code application logic here
+            WarnaKoordinat oo = new WarnaKoordinat(10,4,"Jingga");
+            
+            System.out.println("Warna koordinat : " + oo.getNamaWarna());
+            System.out.println("Koordinat sumbu x : " + oo.getX() + ", y : " + oo.getY());
+            
+            System.out.println();
+        }
+}
---
+++ WarnaKoordinat.java
@@ -0,0 +1,23 @@
+/**
+ * Latihan54
+ * Nama     : Muhammad Fauzan Fadhlulbarr
+ * Kelas    : PBO10K
+ * NIM      : 10119176
+ * Program  : Koordinat
+ */
+public class WarnaKoordinat extends Koordinat{
+    public String namaWarna;
+    
+    public WarnaKoordinat(int x, int y, String namaWarna){
+        super(x,y);
+        this.namaWarna = namaWarna;
+    }
+    
+    public String getNamaWarna(){
+        return namaWarna;
+    }
+    
+    public void setNamaWarna(String namaWarna){
+        this.namaWarna = namaWarna;
+    }
+}
